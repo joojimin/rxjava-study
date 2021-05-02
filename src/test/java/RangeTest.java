ce8e500af318f3d732ba6c251f55766d0fe8949f
jimin.joo <wnwlals22@naver.com>
2021-05-02 19:31:42 +0900

    feat: range test

---
+++ src/test/java/RangeTest.java
@@ -0,0 +1,24 @@
+import io.reactivex.Observable;
+import org.junit.jupiter.api.Test;
+
+public class RangeTest {
+
+	private void print(Object data){
+		String threadName = Thread.currentThread().getName();
+		System.out.println("[" + threadName + "]"
+						   + ", data = " + data);
+	}
+
+	@Test
+	void rangeTest(){
+		Observable.range(1, 10)
+				  .filter(number -> number % 2 == 0)
+				  .doOnNext(this::print)
+				  .subscribe();
+//		2
+//		4
+//		6
+//		8
+//		10
+	}
+}
