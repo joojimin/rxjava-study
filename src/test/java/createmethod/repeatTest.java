546f9a015c0165b56ac7c64feff63f852c30b3f3
jimin.joo <wnwlals22@naver.com>
2021-05-02 22:07:37 +0900

    feat: repeat test

--- src/test/java/createmethod/repeatTest.java
+++ src/test/java/createmethod/repeatTest.java
@@ -3,6 +3,8 @@ package createmethod;
 import io.reactivex.Observable;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
+
 class repeatTest {
 
 	private void print(Object data){
@@ -14,9 +16,28 @@ class repeatTest {
 	@Test
 	void repeatTest(){
 		String[] balls = {"1", "3", "5"};
-		Observable.fromArray(balls)
-				  .repeat(3) // 인자를 안주면 영원히 반복
+		Observable<String> observable =
+			Observable.fromArray(balls)
+				  .repeat(3); // 인자를 안주면 영원히 반복
+
+		observable.subscribe(data -> print("Subscribe #1 = " + data));
+	}
+
+	@Test
+	void heartBeatTest() throws InterruptedException {
+		Observable.timer(1l, TimeUnit.SECONDS)
 				  .doOnNext(this::print)
+				  .repeat(5) // 동작이 한번 끝난 다음에 다시 구독하는 방식으로 동작한다.
 				  .subscribe();
+		Thread.sleep(5000L);
+	}
+
+	@Test
+	void heartBeatTest2() throws InterruptedException {
+		Observable.interval(1L, TimeUnit.SECONDS)
+				  .doOnNext(this::print)
+				  .take(5)
+				  .subscribe();
+		Thread.sleep(5000L);
 	}
 }
